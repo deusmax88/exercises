f6051a181b86350237d2fe27cd7ccbf52f0b0774
Mark <job66-mark@yandex.ru>
2020-05-17 23:06:43 +0500

    Code rearrangement

--- binary-tree/binary-tree-builder-test.php
+++ binary-tree/binary-tree-builder-test.php
@@ -12,34 +12,3 @@ $b = bfsTree($head);
 var_dump($a);
 //var_dump($head);
 var_dump($b);
-
-function bfsTree($head) {
-    if (!$head) {
-        return [];
-    }
-
-    $result = [];
-    $queue = [];
-    $queue[] = $head;
-    while($queue) {
-        $node = array_shift($queue);
-        // If what we've got from our queue is null
-        // because we allowing nulls to be placed
-        // on our queue for non-leaf nodes
-        // we just taking into account that fact
-        if (!$node) {
-            $result[] = null;
-            continue;
-        }
-
-        // We are not in the leaf node
-        if ($node->left != null || $node->right != null) {
-            $queue[] = $node->left;
-            $queue[] = $node->right;
-        }
-
-        $result[] = $node->val;
-    }
-
-    return $result;
-}
