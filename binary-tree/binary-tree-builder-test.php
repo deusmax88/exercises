fa33457e67fd9e7c331bddc2095201a997a76e9c
Mark <job66-mark@yandex.ru>
2020-05-31 22:32:22 +0500

    Correct the comment

--- binary-tree/binary-tree-builder-test.php
+++ binary-tree/binary-tree-builder-test.php
@@ -5,7 +5,7 @@ $a = [4, 2, 7, 1, 3, 6, 9];
 $a = [2, 1, 3, null, 4, null, 7];
 
 $head = buildBinaryTreeFromArray($a);
-// To test that we did your tree building procedure
+// To test that we did our tree building procedure
 // correctly we would do a breadth first traversal
 $b = bfsBinaryTree($head);
 
